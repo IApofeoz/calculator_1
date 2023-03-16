c65584b33548fb14fbbcc107127b7ae135e24bc5
IApofeoz <IApofeoz@DESKTOP-UQOPBVF>
2023-03-17 00:20:02 +0300

    Add project files.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿using System.Drawing;
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button button1;
+            button2 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            panel3 = new Panel();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(67, 76, 94);
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Yellow;
+            button1.Location = new Point(25, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(175, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Вычислить";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(67, 76, 94);
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.Yellow;
+            button2.Location = new Point(25, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(175, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Очистить";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Items.AddRange(new object[] { "2", "3", "4", "5", "6", "7", "8", "9" });
+            comboBox1.Location = new Point(150, 106);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(50, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(25, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Порядок";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackColor = Color.FromArgb(47, 55, 64);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(36, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(225, 146);
+            panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(47, 55, 64);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(36, 197);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(225, 262);
+            panel2.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(135, 143);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(25, 25);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(66, 143);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(25, 25);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(135, 75);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(25, 25);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 75);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(25, 25);
+            textBox1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(47, 55, 64);
+            panel3.Location = new Point(292, 45);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(450, 414);
+            panel3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(46, 52, 64);
+            ClientSize = new Size(784, 511);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Calculator";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Panel panel3;
+    }
+}
